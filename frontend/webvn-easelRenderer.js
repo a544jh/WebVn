ed2bd456a0efe0e7623b2885ecfc68b30ae252ad
a544jh <axel.wikstrom@gmail.com>
2017-03-19 23:36:54 +0200

    testing typewriter effect

--- frontend/webvn-easelRenderer.js
+++ frontend/webvn-easelRenderer.js
@@ -7,11 +7,12 @@ function init() {
   let stage = new createjs.Stage('webvn-canvas');
 
   let player = new WebVn.Player(
-    [new WebVn.TextPrompt([new WebVn.TextNode('asd')])]);
+    [new WebVn.TextPrompt([new WebVn.TextNode(
+      'The quick brown fox jumps over the lazy dog!')])]);
     createjs.Ticker.framerate = 60;
 
-    let nametag = new createjs.Text(player.advance().textNodes[0].text
-    , '16px \'Press Start 2P\''); // maybe get the nametag instaed :P
+    let nametag = new createjs.Text('asd'
+    , '16px \'Press Start 2P\'');
     nametag.x = 5;
     nametag.y = 410;
     let origbounds = nametag.getBounds();
@@ -27,9 +28,40 @@ function init() {
     advg.rect(0, 420, 800, 160);
     adv.alpha = .5;
 
-    stage.addChild(nametag);
-    stage.addChild(bbox);
-    stage.addChild(adv);
+    stage.addChild(nametag, bbox, adv);
+
+    /**
+    * type some text into the adv box!
+    * @param {string} text - text to type
+    */
+    function typeWord(text) {
+      let container = new createjs.Container();
+      stage.addChild(container);
+      let pos = 0;
+      let x = 0; let y = 422;
+      let dtime = 0;
+      createjs.Ticker.addEventListener('tick', function(e) {
+        if ( pos > text.length - 1) {
+          container.removeAllChildren();
+          pos = 0;
+          x = 0;
+          return;
+        }
+        dtime += e.delta;
+        if (dtime < 20) {
+          return;
+        }
+        let ltr = new createjs.Text(text[pos], '16px \'Arial\'');
+        ltr.x = x; ltr.y = y;
+        let bounds = ltr.getBounds();
+        container.addChild(ltr);
+        x += bounds.width;
+        pos += 1;
+        dtime = 0;
+      });
+    }
+
+    typeWord(player.advance().textNodes[0].text);
 
     createjs.Ticker.addEventListener('tick', function() {
       let ticks = createjs.Ticker.getTicks();

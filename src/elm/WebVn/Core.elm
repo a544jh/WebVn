fa17e0dc5330dcc26b181932e4562e9924ccc453
a544jh <axel.wikstrom@gmail.com>
2017-09-03 19:43:23 +0300

    apply elm-format

--- src/elm/WebVn/Core.elm
+++ src/elm/WebVn/Core.elm
@@ -1,87 +1,117 @@
+module Main exposing (..)
+
+
 type alias Player =
-  {
-    story : List Prompt,
-    promptIndex : Int,
-    state : State
-  }
+    { story : List Prompt
+    , promptIndex : Int
+    , state : State
+    }
 
-type Prompt = AnimatablePrompt PromptParams
-  | ControlStructure -- TODO Descision
 
-type alias PromptParams = 
-  {
-    preCmds : List Command,
-    text : Maybe TextPrompt,
-    stop : Bool,
-    label : Maybe String
-  }
+type Prompt
+    = AnimatablePrompt PromptParams
+    | ControlStructure -- TODO Descision
 
-type ControlStructure = 
-  CondintionalJump ToLabel Bool
-  | End
 
-type alias ToLabel = String
+type alias PromptParams =
+    { preCmds : List Command
+    , text : Maybe TextPrompt
+    , stop : Bool
+    , label : Maybe String
+    }
 
-type Command = SpriteCmd | BgCmd
 
-type TextCommand = Command | AppendText TextNode
+type ControlStructure
+    = CondintionalJump ToLabel Bool
+    | End
 
-type alias TextNode = 
-  {
-    text : String,
-    color : String,
-    typeSpeed : Int
-  }
 
-type alias NameTag = Maybe TextNode
+type alias ToLabel =
+    String
 
-type alias TextPrompt = 
-  {
-    style : TextPromptStyle,
-    textCmds : List TextCommand,
-    nameTag : NameTag
-  }
 
-type TextPromptStyle = 
-  Adv
-  | Nvl
-  | Note
-  | Freeform
+type Command
+    = SpriteCmd
+    | BgCmd
+
+
+type TextCommand
+    = Command
+    | AppendText TextNode
+
+
+type alias TextNode =
+    { text : String
+    , color : String
+    , typeSpeed : Int
+    }
+
+
+type alias NameTag =
+    Maybe TextNode
+
+
+type alias TextPrompt =
+    { style : TextPromptStyle
+    , textCmds : List TextCommand
+    , nameTag : NameTag
+    }
+
+
+type TextPromptStyle
+    = Adv
+    | Nvl
+    | Note
+    | Freeform
+
 
 type alias TextBoxState =
-  {
-    style : TextPromptStyle,
-    transitionFrom : Maybe TextPromptStyle,
-    nameTag : NameTag,
-    commands : List TextCommand
-  }
+    { style : TextPromptStyle
+    , transitionFrom : Maybe TextPromptStyle
+    , nameTag : NameTag
+    , commands : List TextCommand
+    }
+
 
 type alias State =
-  {
-    music : (), --TODO
-    background : (), --TODO
-    sprites : List (), --TODO
-    preCommands : List Command,
-    textBox : TextBoxState,
-    sceneName : String
-  }
+    { music : ()
+    , --TODO
+      background : ()
+    , --TODO
+      sprites : List ()
+    , --TODO
+      preCommands : List Command
+    , textBox : TextBoxState
+    , sceneName : String
+    }
+
 
 advance : Player -> Player
 advance player =
-  let
-    nextIndex = player.promptIndex + 1
-    nextPrompt = List.take nextIndex player.story |> List.head
-  in
-    case nextPrompt of
-      Just prompt ->
-        case prompt of
-        AnimatablePrompt params ->
-          {player |
-          state = applyAnimatablePrompt params player.state,
-          promptIndex = nextIndex}
-        ControlStructure -> player --TODO
-      Nothing -> player
+    let
+        nextIndex =
+            player.promptIndex + 1
+
+        nextPrompt =
+            List.take nextIndex player.story |> List.head
+    in
+        case nextPrompt of
+            Just prompt ->
+                case prompt of
+                    AnimatablePrompt params ->
+                        { player
+                            | state = applyAnimatablePrompt params player.state
+                            , promptIndex = nextIndex
+                        }
+
+                    ControlStructure ->
+                        player
+
+            --TODO
+            Nothing ->
+                player
+
 
 applyAnimatablePrompt : PromptParams -> State -> State
-applyAnimatablePrompt prompt state = 
-  state
+applyAnimatablePrompt prompt state =
+    state

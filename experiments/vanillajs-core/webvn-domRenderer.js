a4da9188c3ebc68e520915a0a0596b4d1ab631e9
a544jh <axel.wikstrom@gmail.com>
2018-02-17 00:23:11 +0200

    some more experimenting...

--- experiments/vanillajs-core/webvn-domRenderer.js
+++ experiments/vanillajs-core/webvn-domRenderer.js
@@ -7,14 +7,33 @@ let player = new WebVn.Player(tree);
 * setText
 */
 function advance() {
-  textbox.innerHTML = JSON.stringify(player.advance());
+  transitionDelay(player.advance().textNodes.text) // lol need typescript
 }
 
 let body = document.body;
 let textbox = document.createElement('div');
-textbox.innerHTML = JSON.stringify(player.getCurrentPrompt());
 body.appendChild(textbox);
 let button = document.createElement('button');
 button.innerHTML = 'Next';
 button.addEventListener('click', advance);
 body.appendChild(button);
+
+
+function transitionDelay(text) {
+  const delay = 20
+
+  textbox.innerHTML = ""
+  for (let i = 0; i < text.length; i++) {
+    let span = document.createElement("span")
+    span.innerText = text.charAt(i)
+    span.style.opacity = "0"
+    span.style.transitionProperty = "opacity"
+    span.style.transitionDelay = (i * delay) + "ms"
+    textbox.appendChild(span)
+  }
+  window.setTimeout(function () {
+    for (let i = 0; i < textbox.children.length; i++) {
+      textbox.children[i].style.opacity = "1"
+    }
+  }, 0)
+}

ba72b6154bde99de8af2869ff805c40129dd1b29
a544jh <axel.wikstrom@gmail.com>
2018-02-22 00:15:10 +0200

    playing around with ts interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import './index.html';
+import { AnimatablePrompt, TextNode, PromptType } from './prompt';
 
 document.write("Hello from TypeScript!")
+
+let prompt: AnimatablePrompt = {type: PromptType.ADV, textNodes: []}
+
+let textNode: TextNode = {text: "asd"}
+
+prompt.textNodes = [textNode]
+
+document.write(JSON.stringify(prompt))

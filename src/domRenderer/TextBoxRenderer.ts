5057bc9819991661a4933ff6e8f4092aab278d50
Axel Wikström <axel.wikstrom@gmail.com>
2021-04-22 23:42:10 +0300

    fix click to skip animation bug, some refactoring

--- src/domRenderer/TextBoxRenderer.ts
+++ src/domRenderer/TextBoxRenderer.ts
@@ -1,7 +1,6 @@
 import { ADVNameTag, ADVTextBox, TextBox, TextBoxType } from "../core/state"
 
 export class TextBoxRenderer {
-
   private root: HTMLDivElement
   private advTextBox: HTMLDivElement
   private advNameTag: HTMLDivElement
@@ -18,39 +17,40 @@ export class TextBoxRenderer {
     this.advNameTag.classList.add("vn-adv-nametag")
   }
 
-  public render(prevText: TextBox | null, text: TextBox | null, animate: boolean): Promise<void> {
+  public render(prevTextBox: TextBox | null, textBox: TextBox | null, animate: boolean): Promise<void> {
+    if (animate && textBox === prevTextBox) {
+      return Promise.resolve()
+    }
+    // if we're NOT animating, we want to go ahead and clear the event listners
 
-    if (text === null) {
+    if (textBox === null) {
       // TODO: exit animation...
       this.root.innerHTML = ""
       // TODO
       return Promise.resolve()
     }
 
-    if (text === prevText) {
-      return Promise.resolve()
-    }
-
     // maybe use dict to lookup, or class instance method....
-    switch (text.type) {
+    switch (textBox.type) {
       case TextBoxType.ADV:
-        return this.renderAdv(prevText, text as ADVTextBox, animate)
+        return this.renderAdv(prevTextBox, textBox as ADVTextBox, animate)
     }
 
     return Promise.resolve()
   }
 
   private async renderAdv(prevAdv: ADVTextBox | null, adv: ADVTextBox, animate: boolean): Promise<void> {
-
     let resolveAnimationFinished: (value?: unknown | PromiseLike<unknown> | undefined) => void
-    const animationFinished = new Promise<void>((resolve) => { resolveAnimationFinished = resolve })
+    const animationFinished = new Promise<void>((resolve) => {
+      resolveAnimationFinished = resolve
+    })
 
     if (!this.root.contains(this.advTextBox)) {
       this.root.appendChild(this.advTextBox)
     }
-    this.advTextBox.innerHTML = ""
+    this.advTextBox.innerHTML = "" // this will clear any pending event listners?
 
-    const prevNameTag = (prevAdv === null ? undefined : prevAdv.nameTag)
+    const prevNameTag = prevAdv === null ? undefined : prevAdv.nameTag
     await this.renderAdvNameTag(prevNameTag, adv.nameTag, animate)
 
     const fragment = document.createDocumentFragment()
@@ -93,18 +93,34 @@ export class TextBoxRenderer {
   private async renderAdvNameTag(
     prevNameTag: ADVNameTag | undefined,
     nameTag: ADVNameTag | undefined,
-    animate: boolean): Promise<unknown> {
+    animate: boolean
+  ): Promise<unknown> {
+    let resolveAnimationFinished: (value?: unknown | PromiseLike<unknown> | undefined) => void = () => {
+      return
+    }
+    const animationFinished = new Promise((resolve) => {
+      resolveAnimationFinished = resolve
+    })
 
-    let resolveAnimationFinished: (value?: unknown | PromiseLike<unknown> | undefined) => void = () => { return }
-    const animationFinished = new Promise((resolve) => { resolveAnimationFinished = resolve })
-
-    const remove = () => {
-      this.advNameTag.removeEventListener("transitionend", remove)
+    const removeFromDom = () => {
+      this.advNameTag.removeEventListener("transitionend", removeFromDom)
       this.root.removeChild(this.advNameTag)
       resolveAnimationFinished()
     }
 
-    if(!animate) {
+    const changeNameTransition = () => {
+      this.advNameTag.removeEventListener("transitionend", changeNameTransition)
+      setNameTagProps()
+      this.advNameTag.style.transform = "scaleY(1)"
+      this.advNameTag.addEventListener("transitionend", finishTransition)
+    }
+
+    const finishTransition = () => {
+      this.advNameTag.removeEventListener("transitionend", finishTransition)
+      resolveAnimationFinished()
+    }
+
+    if (!animate) {
       // remove event listeners to prevent race conditions...
       const clone = this.advNameTag.cloneNode(true) as HTMLDivElement
       this.advNameTag.replaceWith(clone)
@@ -116,7 +132,7 @@ export class TextBoxRenderer {
       if (this.root.contains(this.advNameTag)) {
         if (animate) {
           this.advNameTag.style.transform = "scaleY(0)"
-          this.advNameTag.addEventListener("transitionend", remove)
+          this.advNameTag.addEventListener("transitionend", removeFromDom)
         } else {
           this.root.removeChild(this.advNameTag)
           resolveAnimationFinished()
@@ -142,17 +158,6 @@ export class TextBoxRenderer {
       return
     }
 
-    const changeNameTransition = () => {
-      this.advNameTag.removeEventListener("transitionend", changeNameTransition)
-      setNameTagProps()
-      this.advNameTag.style.transform = "scaleY(1)"
-      this.advNameTag.addEventListener("transitionend", finishTransition)
-    }
-
-    const finishTransition = () => {
-      this.advNameTag.removeEventListener("transitionend", finishTransition)
-      resolveAnimationFinished()
-    }
     // enter
     if (prevNameTag === undefined) {
       this.advNameTag.style.transform = "scaleY(0.0001)" // can't be 0 because of firefox lol
@@ -161,7 +166,8 @@ export class TextBoxRenderer {
       this.advNameTag.style.transform = "scaleY(1)"
       this.advNameTag.addEventListener("transitionend", finishTransition)
       // swap
-    } else if (prevNameTag.name !== nameTag.name || prevNameTag.color !== nameTag.color) { // TODO: deep compare
+    } else if (prevNameTag.name !== nameTag.name || prevNameTag.color !== nameTag.color) {
+      // TODO: deep compare
       this.advNameTag.style.transform = "scaleY(0.0001)"
       this.advNameTag.addEventListener("transitionend", changeNameTransition)
     } else {
@@ -171,4 +177,3 @@ export class TextBoxRenderer {
     return animationFinished
   }
 }
-
